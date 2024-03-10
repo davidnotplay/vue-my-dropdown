ccb39c546033dccde45a29cf6abd196462dc8f89
David Casado <dcasadomartinez@gmail.com>
2024-03-10 14:51:49 +0100

    Config (test): Add new features in config system

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import {defineConfig, mergeConfig} from 'vitest/config';
+import viteConfig from './vite.config';
 
-export default mergeConfig(viteConfig, defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-}));
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: ['./vitest-setup.ts'],
+        },
+    })
+);
