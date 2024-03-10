8c28fb66734b9273cd8ea219f391be639bdeffd4
David Casado <dcasadomartinez@gmail.com>
2024-03-10 13:36:10 +0100

    Config (core): Create test structure

---
+++ src/vue-my-dropdown/BaseDropdown.vue
@@ -0,0 +1,3 @@
+<template>
+  <h3>Version 3.0.0 starts here</h3>
+</template>
---
+++ src/vue-my-dropdown/__tests__/BaseDropdown.test.ts
@@ -0,0 +1,7 @@
+import {it, expect} from 'vitest';
+import Dropdown from '../../vue-my-dropdown/BaseDropdown.vue';
+
+
+it('exists', () => {
+  expect(Dropdown).toBeTruthy();
+});
