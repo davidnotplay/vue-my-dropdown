94be0b3b9e3ba26579e9c43eb17a331312cd7969
David Casado <dcasadomartinez@gmail.com>
2024-03-10 02:35:17 +0100

    Config (core): Install vite

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,30 @@
-<template>
-  <div id="app">
-    <dropdown :visible="visible" @clickout="visible = false" @positioned="showPosition">
-      <span @click="visible = !visible" class="click-here">Click here</span>
-      <div class="dialog" slot="dropdown">dialog</div>
-    </dropdown>
- </div>
-</template>
-
-<script>
-import dropdown from './vue-my-dropdown'
-
-export default {
-  name: 'app',
-
-  data() {
-    return { visible: false }
-  },
-
-  components: { dropdown },
-
-  methods: {
-    showPosition(r) {
-      console.log(r)
-    }
-  }
-}
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-.click-here {
-  display: inline-block;
-  padding: 3px;
-  cursor: pointer;
-  box-sizing: border-box;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-.dialog{
-  background: #eee;
-  border: 1px solid #ccc;
-  padding: .8rem;
-  box-shadow: 1px 1px 6px 0 #999;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
