bdac8d7968744730ce9356baa13ca06726fa33e9
David Casado <dcasadomartinez@gmail.com>
2024-08-07 15:46:37 +0200

    Feature (BaseDropdown): Create component. First approach

--- src/vue-my-dropdown/BaseDropdown.vue
+++ src/vue-my-dropdown/BaseDropdown.vue
@@ -1,3 +1,69 @@
+<script setup lang="ts">
+import {onMounted, ref, watch} from 'vue';
+import {createStyles} from './helpers.ts';
+
+export type BaseDropdownProps = {
+    visible?: boolean;
+    position?: Position;
+};
+
+export type Position = [Horizontal, Vertical, Horizontal, Vertical];
+export type Vertical = 'top' | 'center' | 'bottom';
+export type Horizontal = 'left' | 'center' | 'right';
+
+const props = withDefaults(defineProps<BaseDropdownProps>(), {
+    visible: false,
+    position: () => ['right', 'top', 'left', 'top'],
+});
+
+// Elements
+const $link = ref(null);
+const $dropdown = ref(null);
+
+// Internal states
+const ddStyles = ref<{[key: string]: string}>({});
+
+defineSlots<{
+    dropdown(): any; // eslint-disable-line
+    default(): any; // eslint-disable-line
+}>();
+
+// Helpers
+function open() {
+    if ($link.value === null || $dropdown.value === null) {
+        return;
+    }
+
+    ddStyles.value = createStyles($link.value, $dropdown.value, props.position);
+}
+
+// Watchers
+watch(
+    () => props.visible,
+    async (newValue, oldValue) => {
+        if (newValue == oldValue) {
+            return;
+        }
+
+        if (newValue) {
+            open();
+        }
+    }
+);
+
+// Lifecycle
+onMounted(() => {
+    if (props.visible) {
+        open();
+    }
+});
+</script>
+
 <template>
-  <h3>Version 3.0.0 starts here</h3>
+    <span ref="$link" :style="{display: 'inline-block'}">
+        <slot />
+    </span>
+    <div v-show="visible" ref="$dropdown" :style="ddStyles">
+        <slot name="dropdown" />
+    </div>
 </template>
--- src/vue-my-dropdown/__tests__/BaseDropdown.test.ts
+++ src/vue-my-dropdown/__tests__/BaseDropdown.test.ts
@@ -1,7 +1,101 @@
-import {it, expect} from 'vitest';
+import {cleanup, fireEvent, render, screen} from '@testing-library/vue';
 import Dropdown from '../../vue-my-dropdown/BaseDropdown.vue';
+import {nextTick} from 'vue';
 
+beforeEach(() => {
+    cleanup();
+    Object.defineProperty(HTMLElement.prototype, 'offsetParent', {
+        get() {
+            return document.querySelector('body');
+        },
+    });
+});
 
 it('exists', () => {
-  expect(Dropdown).toBeTruthy();
+    expect(Dropdown).toBeTruthy();
+});
+
+it('displays default slot', () => {
+    const TestComponent = {
+        template: `<Dropdown><div>Default slot</div></Dropdown>`,
+        components: {Dropdown},
+    };
+
+    render(TestComponent);
+    expect(screen.getByText(/Default slot/)).toBeInTheDocument();
+});
+
+it('hides `dropdown` slot by default', () => {
+    const TestComponent = {
+        template: `
+<Dropdown>
+    <div>Default slot</div>
+    <template v-slot:dropdown>Dropdown message</template>
+</Dropdown>
+`,
+        components: {Dropdown},
+    };
+
+    render(TestComponent);
+    expect(screen.getByText(/Dropdown message/)).not.toBeVisible();
+});
+
+it('shows `dropdown` slot when the `visible` property is true', () => {
+    const TestComponent = {
+        template: `
+<Dropdown visible>
+    <div>Default slot</div>
+    <template v-slot:dropdown>Dropdown message</template>
+</Dropdown>
+`,
+        components: {Dropdown},
+    };
+
+    render(TestComponent);
+    expect(screen.getByText(/Dropdown message/)).toBeVisible();
+});
+
+it('executes `open` function when the initial value of `visible` property is true', async () => {
+    const TestComponent = {
+        template: `
+<Dropdown visible>
+    <div>Default slot</div>
+    <template v-slot:dropdown>
+        <div data-testid="dropdown">
+            Dropdown message
+        </div>
+    </template>
+</Dropdown>
+`,
+        components: {Dropdown},
+    };
+
+    render(TestComponent);
+    await nextTick();
+    expect(screen.getByTestId('dropdown')!.parentElement!.style!.position).toBe('absolute');
+});
+
+it('executes `open` function when the `visible` property changes to `true` value', async () => {
+    const TestComponent = {
+        template: `
+        <Dropdown :visible>
+            <button type="button" @click="visible = true">show {{visible}}</button>
+            <template v-slot:dropdown>
+                <div data-testid="dropdown">
+                    Dropdown message
+                </div>
+            </template>
+        </Dropdown>
+`,
+        components: {Dropdown},
+        data() {
+            return {visible: false};
+        },
+    };
+
+    render(TestComponent);
+    await nextTick();
+    await fireEvent.click(screen.getByRole('button'));
+    await nextTick();
+    expect(screen.getByTestId('dropdown')!.parentElement!.style!.position).toBe('absolute');
 });
