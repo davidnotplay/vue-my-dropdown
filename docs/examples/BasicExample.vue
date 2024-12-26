2aaf68725623b46016f962e5aa5fe94702320a36
David Casado <dcasadomartinez@gmail.com>
2024-12-26 23:30:01 +0100

    Compile docs

---
+++ docs/examples/BasicExample.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import {ref} from 'vue';
+import {MyDropdown} from '../../src/vue-my-dropdown';
+
+const visible = ref<boolean>(false);
+const anchor = ref<HTMLElement | null>(null);
+</script>
+
+<template>
+    <button ref="anchor" type="button" class="my-button" @click="visible = !visible">
+        Click me
+    </button>
+    <MyDropdown :anchor="anchor" :visible="visible">
+        <div class="my-dropdown">My first dropdown</div>
+    </MyDropdown>
+</template>
---
+++ docs/examples/ClickoutExample.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import {ref} from 'vue';
+import {MyDropdown} from '../../src/vue-my-dropdown';
+
+const visible = ref<boolean>(false);
+const anchor = ref<HTMLElement | null>(null);
+
+function clickout(_: Event, clickedInDropdown: boolean, clickedInAnchor: boolean) {
+    visible.value = clickedInDropdown || clickedInAnchor;
+}
+</script>
+
+<template>
+    <button ref="anchor" type="button" class="my-button" @click="visible = !visible">
+        Click me
+    </button>
+    <MyDropdown :anchor="anchor" :visible="visible" @clickout="clickout">
+        <div class="my-dropdown">Click out to close the dropdown</div>
+    </MyDropdown>
+</template>
